b5c5c08210b0f4c99ebb5db08267bb3e2ab65d71
Florentin Anggraini Purnama <florentin@electrum.id>
2024-01-19 22:01:22 +0700

    fix

--- src/L1_Main.java
+++ src/L1_Main.java
@@ -1,5 +1,8 @@
 public class L1_Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int myNum = Integer.parseInt(null);
+        Integer myNumasdf = null;
+        System.out.println(myNumasdf);
     }
 }
---
+++ src/composition/src/Main.java
@@ -0,0 +1,8 @@
+package composition.src;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
